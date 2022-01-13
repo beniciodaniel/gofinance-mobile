b9a6ff9d2d1aa105a40f8b37cfde4c56d1d55270
Benício Barreto <benicio.daniel@gmail.com>
2022-01-12 21:44:36 -0300

    created global theme

---
+++ src/global/styles/theme.ts
@@ -0,0 +1,19 @@
+export default {
+  colors: {
+    primary: '#8257e5',
+
+    secondary: '#ff7a29',
+    secondary_light: 'rgba(255, 122, 41, 0.3)',
+
+    success: '#04d361',
+    success_light: 'rgba(4, 211, 97, 0.3)',
+
+    attention: '#ce4a4a',
+    attention_light: '#eb3223',
+
+    shape: '#fff',
+    title: '#363f5f',
+    text: '#a8a8b3',
+    background: '#121214'
+  }
+};
