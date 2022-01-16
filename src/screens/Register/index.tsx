57824db550a854a29a1a78f85cda6943dad84cdb
Benício Barreto <benicio.daniel@gmail.com>
2022-01-16 11:08:22 -0300

    added basic Button component to Register

--- src/screens/Register/index.tsx
+++ src/screens/Register/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
+import { Button } from '../../components/Forms/Button';
 import { Input } from '../../components/Forms/Input';
-import { Container, Header, Title, Form } from './styles';
+import { Container, Header, Title, Form, Fields } from './styles';
 
 export function Register() {
   return (
@@ -10,8 +11,11 @@ export function Register() {
       </Header>
 
       <Form>
-        <Input placeholder="Nome" />
-        <Input placeholder="Nome" />
+        <Fields>
+          <Input placeholder="Nome" />
+          <Input placeholder="Preço" />
+        </Fields>
+        <Button title="Enviar" />
       </Form>
     </Container>
   );
